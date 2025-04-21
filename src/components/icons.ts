befdf3b52aa41eb9b475b5647dc6ea9076d25315
hussain abbas <hussainabbas2000@gmail.com>
2025-04-21 23:23:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/components/icons.ts:61:10
    > Parsing ecmascript source code failed
    >   59 |   user: User,
    >   60 |   logo: () => (
    > > 61 |     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
    >      |          ^^^^^
    >   62 |       <path d="M3 3H21V21H3V3Z" fill="currentColor"/>
    >   63 |       <path d="M7 7H17V9H7V7Z" fill="black"/>
    >   64 |       <path d="M7 11H13V13H7V11Z" fill="black"/>
    >
    > Expected '>', got 'xmlns'
    >
    >

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -27,6 +27,7 @@ import {
   X,
   Workflow,
   Search,
+  LayoutDashboard,
 } from 'lucide-react';
 
 const Icons = {
@@ -57,14 +58,7 @@ const Icons = {
   spinner: Loader2,
   trash: Trash,
   user: User,
-  logo: () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-      <path d="M3 3H21V21H3V3Z" fill="currentColor" />
-      <path d="M7 7H17V9H7V7Z" fill="black" />
-      <path d="M7 11H13V13H7V11Z" fill="black" />
-      <path d="M7 15H17V17H7V15Z" fill="black" />
-    </svg>
-  ),
+  logo: LayoutDashboard,
 };
 
 export {Icons};
