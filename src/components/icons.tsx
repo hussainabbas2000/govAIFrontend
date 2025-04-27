50c10f05f94af46dd8dad5a49c602c556b495ca6
hussain abbas <hussainabbas2000@gmail.com>
2025-04-27 12:18:55 +0000

    Make it to lightest blue

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -39,8 +39,9 @@ import {
 } from 'lucide-react';
 
 // Define SVG components directly for Search and Logo
-const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => (
+const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => ( 
   <svg
+    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
@@ -58,6 +59,7 @@ const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 const LogoIcon = (props: React.SVGProps<SVGSVGElement>) => (
  <svg
+    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
